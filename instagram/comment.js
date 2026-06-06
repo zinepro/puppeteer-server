634b18e4b23af2aabe6d709fbde2f19382a7a2b0
Zinedine <z_sidane@estin.com>
2026-06-07 00:37:20 +0100

    ajout timeout

--- instagram/comment.js
+++ instagram/comment.js
@@ -3,7 +3,8 @@ async function postComments(page, url, comment, count) {
   await new Promise((r) => setTimeout(r, 3000));
 
   for (let i = 0; i < count; i++) {
-    await page.waitForSelector("textarea[placeholder]", { timeout: 10000 });
+    await page.waitForNetworkIdle({ timeout: 30000 });
+    await page.waitForSelector("textarea[placeholder]", { timeout: 60000 });
     await page.click("textarea[placeholder]");
     await new Promise((r) => setTimeout(r, 500));
     await page.type("textarea[placeholder]", comment, { delay: 50 });
